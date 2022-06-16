02997287eae230178069757d1d7522c0766c2286
elenatruniak <elena.truniak@gmail.com>
2022-06-16 13:03:53 +0200

    fixed failed test

--- test_google.py
+++ test_google.py
@@ -8,6 +8,6 @@ def test_search_element(test_open_google):
     print("link is found")
 
 def test_failed_search_element(test_open_google):
-    browser.element('[name="q"]').should(be.blank).type('selene').press_enter()
+    browser.element('[name="q"]').should(be.blank).type('python').press_enter()
     browser.element('[id="search"]').should_not(have.text('User-oriented Web UI browser tests in Python'))
     print("link wasn't found")
